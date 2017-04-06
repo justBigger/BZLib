99cb9e779636edfa2c2b1d3830bd64c4fc001f47
tmp <tmp@phone-wifi.local>
2017-04-06 11:24:11 +0800

    clean

--- Example/Pods/Target Support Files/BZLib/BZLib-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_BZLib : NSObject
-@end
-@implementation PodsDummy_BZLib
-@end
--- Example/Pods/Target Support Files/Pods-BZLib_Example/Pods-BZLib_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_BZLib_Example : NSObject
-@end
-@implementation PodsDummy_Pods_BZLib_Example
-@end
